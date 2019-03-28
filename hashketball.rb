c88f36249e6918dd2c3c0c0bba0a4812a9751fd7
Sarah Elzinga <sevans924@gmail.com>
2019-03-28 16:07:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,7 +163,7 @@ end
 def player_numbers(team)
   game_hash.map do |home_or_away, team_data|
       team_data.map do |player_name, stats|
-        binding.pry
+    
         stats[:number]
       end
     #end
